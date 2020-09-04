ad8cd5ba2a73797b22fef09be182e388e7cda65f
vinuthna <allavs01@gmail.com>
2020-09-04 22:18:45 +0530

    Updates

---
+++ precache-manifest.4255308ab6357c2d2611ad89d9256da6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b36df009675c7dd86845653b7799833",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5c738ed76e40f47e999c",
+    "url": "/Portfolio/static/css/main.5b195ef4.chunk.css"
+  },
+  {
+    "revision": "4ee9b633ea0a30454dd0",
+    "url": "/Portfolio/static/js/2.2ab27cf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.2ab27cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c738ed76e40f47e999c",
+    "url": "/Portfolio/static/js/main.bebd8bc1.chunk.js"
+  },
+  {
+    "revision": "dad1d295beb3186b5152",
+    "url": "/Portfolio/static/js/runtime-main.10e94478.js"
+  },
+  {
+    "revision": "5e9476d21f7d2aa03a5fcd33b53c545e",
+    "url": "/Portfolio/static/media/1.5e9476d2.png"
+  },
+  {
+    "revision": "bb4ed9a8e79a73976dd871b7f10d4e3f",
+    "url": "/Portfolio/static/media/2.bb4ed9a8.png"
+  },
+  {
+    "revision": "e1ac101cbe0487e0f2b320573595eecb",
+    "url": "/Portfolio/static/media/3.e1ac101c.png"
+  },
+  {
+    "revision": "2e58bea0ca6db00800264f94586bc672",
+    "url": "/Portfolio/static/media/4.2e58bea0.png"
+  },
+  {
+    "revision": "eb4acc915876e09ac8a0230d4337beb9",
+    "url": "/Portfolio/static/media/5.eb4acc91.png"
+  },
+  {
+    "revision": "6848c847d268174d190be90758e86f99",
+    "url": "/Portfolio/static/media/6.6848c847.png"
+  },
+  {
+    "revision": "75a47a4fad80cf91f924e7077ecd8485",
+    "url": "/Portfolio/static/media/GitHub.75a47a4f.svg"
+  },
+  {
+    "revision": "d8bcc7ef69ee5836f34775b5c56ea6f4",
+    "url": "/Portfolio/static/media/Hacker.d8bcc7ef.svg"
+  },
+  {
+    "revision": "28facd305af67eb314a86f3734c14eeb",
+    "url": "/Portfolio/static/media/Lang.28facd30.svg"
+  },
+  {
+    "revision": "fb49ea20b58b5077682a8794063f7c81",
+    "url": "/Portfolio/static/media/LangHori.fb49ea20.svg"
+  },
+  {
+    "revision": "5f89fb8732ed77249e7d11cca69c79ed",
+    "url": "/Portfolio/static/media/LinkDin.5f89fb87.svg"
+  },
+  {
+    "revision": "c9423e763ac72b17b8dd66ecc056fb6b",
+    "url": "/Portfolio/static/media/about.c9423e76.png"
+  },
+  {
+    "revision": "1469996131a0c0f84bcc32e5680a9e72",
+    "url": "/Portfolio/static/media/home.14699961.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c87ea1f5473a9abd54e438eedf64eb1",
+    "url": "/Portfolio/static/media/me.9c87ea1f.jpg"
+  },
+  {
+    "revision": "d8b942a9dcda5363c83cc7239ae8969f",
+    "url": "/Portfolio/static/media/other.d8b942a9.png"
+  },
+  {
+    "revision": "8f2634df23c6784b9cdfc89be7d6dd12",
+    "url": "/Portfolio/static/media/researchFlow.8f2634df.png"
+  }
+]);
